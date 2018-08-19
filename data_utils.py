b66b6e0e51be803f200a0fea826d9ff180446daa
richard sun <topsun888@yahoo.com>
2018-08-19 14:09:51 +0800

    Complete a pipieline for LM+SentimentAnalysis

--- data_utils.py
+++ data_utils.py
@@ -6,6 +6,71 @@ class data_helper:
     '''
     Read dataset, tokenize sentences and map words into idx
     '''
+    def __init__(self, conf):
+        self.conf = conf
+        
+    def read_words(self, conf):
+        '''Read 1 billion words'''
+        words = []
+        for file in os.listdir(conf.data_dir):
+            with open(os.path.join(conf.data_dir, file), 'r') as f:
+                for line in f.readlines():
+                    tokens = line.split()
+                    # NOTE Currently, only sentences with a fixed size are chosen
+                    # to account for fixed convolutional layer size.
+                    if len(tokens) == conf.context_size-2:
+                        words.extend((['<pad>']*int(conf.filter_h/2)) + ['<s>'] + tokens + ['</s>'])
+        return words
+    
+    def index_words(self, words, conf):
+        word_counter = collections.Counter(words).most_common(conf.vocab_size-1)
+        word_to_idx = {'<unk>': 0}
+        idx_to_word = {0: '<unk>'}
+        for i,_ in enumerate(word_counter):
+            word_to_idx[_[0]] = i+1
+            idx_to_word[i+1] = _[0]
+        data = []
+        for word in words:
+            idx = word_to_idx.get(word)
+            idx = idx if idx else word_to_idx['<unk>']
+            data.append(idx)
+        return np.array(data), word_to_idx, idx_to_word
+    
+    def create_batches(self, data, conf):
+        conf.num_batches = int(len(data) / (conf.batch_size * (conf.text_size+1)))
+        size = int(conf.num_batches * conf.batch_size * (conf.text_size+1))
+        #print(size)
+        data = data[:size]
+        xdata = data
+        ydata = np.copy(data)
+
+        ydata[:-1] = xdata[1:]
+        ydata[-1] = xdata[0]
+        x_batches = np.split(xdata.reshape(conf.batch_size, -1), conf.num_batches, 1)
+        y_batches = np.split(ydata.reshape(conf.batch_size, -1), conf.num_batches, 1)
+
+        for i in np.arange(conf.num_batches):
+            x_batches[i] = x_batches[i][:,:-1]
+            y_batches[i] = y_batches[i][:,:-1]
+        return x_batches, y_batches
+    
+    def get_batch(self, x_batches, y_batches, batch_idx):
+        x, y = x_batches[batch_idx], y_batches[batch_idx]
+        batch_idx += 1
+        if batch_idx >= len(x_batches):
+            batch_idx = 0
+        return x, y.reshape(-1,1), batch_idx
+    
+    
+    def prepare_data(self):
+        words = read_words(self.conf)
+        data, word_to_idx, idx_to_word = self.index_words(words, self.conf)
+        x_batches, y_batches = self.create_batches(data, self.conf)
+
+        del words
+        del data
+
+        return x_batches, y_batches, word_to_idx, idx_to_word
 
 def read_words1(conf):
     words = []
---
+++ sents_handler.py
@@ -0,0 +1,86 @@
+import numpy as np
+import spacy
+nlp = spacy.load('en')
+class generate_samples:
+    '''
+    Generate samples of training data or testing data for data analysis
+    '''
+    def __init__(self, data, labels, word_to_idx, max_sent_len=30, is_training=True):
+        '''
+        Args:
+        data: numpy
+        labels: numpy
+        '''
+        self.data = data
+        self.labels = labels
+        self.word_to_idx = word_to_idx
+        self.is_training = is_training
+        self.max_sent_len = max_sent_len
+        self.index = 0
+        
+    def sent_split(self, sent):
+        '''
+        Split a sentence into tokens
+        '''
+        words = []
+        sent = nlp(sent.strip())
+        for w in sent:
+            words.append(w.text.lower())
+        return words
+        
+    def generate_samples(self, sents, labels, batch_size=64):
+        '''
+        Select a batch_size of sentences
+        Transform each sentence into a sequence of idx
+        '''
+        indice = np.random.choice(len(sents), batch_size)
+        sents = sents[indice]
+        labels = labels[indice]
+        sent_vecs, sent_lens = self.create_sent_idx(sents)
+        return sent_vecs, labels, sent_lens
+    
+    
+    def create_sent_idx(self, sents):
+        '''
+        Map sents into idx
+        '''
+        sents_lens = list(map(self.sent2idx, sents))
+        sents_idx, sents_lens = list(zip(*sents_lens)) 
+        return sents_idx, sents_lens
+        
+        
+    def sent2idx(self, sent):
+        '''Map a sentence into a sequence of idx'''
+        sent_idx = []
+        words = self.sent_split(str(sent))
+        lens = len(words)
+        ##Cut long sentences
+        if lens > self.max_sent_len:
+            words = words[:self.max_sent_len]
+            lens = self.max_sent_len
+        for w in words:
+            idx = self.word_to_idx.get(w)
+            idx = idx if idx else self.word_to_idx['<unk>']
+            sent_idx.append(idx)
+        ###Pad short sentences
+        for i in np.arange(lens, self.max_sent_len):
+            idx = self.word_to_idx['<pad>']
+            sent_idx.append(idx)
+        return sent_idx, lens
+    
+    def generate(self, batch_size=64):
+        if self.is_training:
+            sent_vecs, sent_labels, lengths = self.generate_samples(self.data, 
+                                                               self.labels,
+                                                              batch_size)
+        else:
+            start = self.index
+            end = start + batch_size
+            if end > len(self.data):
+                print('Out of sample size')
+                self.index = 0
+            sents = self.data[start:end]
+            sent_labels = self.labels[start:end]
+            sent_vecs, lengths = self.create_sent_idx(sents)
+            self.index = end
+        return sent_vecs, sent_labels, lengths
