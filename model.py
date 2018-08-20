02e98312c9a35b88139a6e4ac3ae7681cfb7f3d4
richard sun <topsun888@yahoo.com>
2018-08-20 21:30:19 +0800

    Improved Gated CNN model

--- model.py
+++ model.py
@@ -14,20 +14,28 @@ class GatedCNN(object):
         #Initialize the input of each layer
         self.internal_states = []
         
-        #h, res_input = embed, embed
-        
-        shape = (1, conf.filter_h, conf.filter_w, 1)
-        
-        h = self.glu(embed, shape, conf)
+        h, res_input = embed, embed
+        for i in np.arange(conf.num_layers):
+            self.internal_states.append(h)
+            shape = (1, conf.filter_h, conf.filter_w, 1)
+            h = self.glu(h, shape, conf, i)
+            if i % conf.block_size == 2:
+                h += res_input
+                res_input = h
         
 
         #Flatten the output, (batch_size*max_len) * emb_size
         #print(h)
         self.out_layer = tf.squeeze(h, 1)
         #h = tf.reshape(h, (-1, conf.embedding_size))
+        
         h = tf.reshape(h, (-1, conf.embedding_size))
         
-        print(h)
+        #residual layer, prevent weight diminishing
+        #Note residual block should avoid the last layer
+        
+        
+        #print(h)
         y_shape = self.y.get_shape().as_list()
         #Flatten the label, (batch_size*max_len) * 1
         self.y = tf.reshape(self.y, (y_shape[0] * y_shape[1], 1))
@@ -65,42 +73,33 @@ class GatedCNN(object):
 
         self.create_summaries()
         
-    def glu(self, embed, shape, conf):
+    def glu(self, inputs, shape, conf, layer_index):
         #Initialize kernel size
         height = conf.filter_h
         width = conf.filter_w
-        h, res_input = embed, embed
+        #h, res_input = embed, embed
         kernel_width = shape[1]
         left_pad = kernel_width - 1
         paddings = [[0,0],[0,0],[left_pad,0],[0,0]]
-        padded_input = tf.pad(h, paddings, "CONSTANT")
+        padded_input = tf.pad(inputs, paddings, "CONSTANT")
 
-        for i in range(conf.num_layers):
-            #Record internal states
-
-            
-            with tf.variable_scope("layer_%d"%i):
-                #Linear layer
-                conv_w = self.conv_op(padded_input, shape, "linear")
-                #conv_w = tf.layers.conv2d(h, filter_size,
-                                       #kernel_size=(height, width),
-                                       #strides=(1, width), padding='same',
-                                        #name='linear')
-                #Gate layer
-                conv_v = self.conv_op(padded_input, shape, "gated")
-                #conv_v = tf.layers.conv2d(h, filter_size,
-                                       #kernel_size=(height, width),
-                                       #strides=(1, width), padding='same',
-                                        #name='gate')
-                #Elementwise multiplication
-                #batch_size, max_len, 1, filter_size
-                h = conv_w * tf.sigmoid(conv_v)
-                #print(h)
-                #residual layer, prevent weight diminishing
-                #Note residual block should avoid the last layer
-                if i % conf.block_size == 1:
-                    h += res_input
-                    res_input = h
+        with tf.variable_scope("layer_%d"%layer_index):
+            #Linear layer
+            conv_w = self.conv_op(padded_input, shape, "linear")
+            #conv_w = tf.layers.conv2d(h, filter_size,
+                                   #kernel_size=(height, width),
+                                   #strides=(1, width), padding='same',
+                                    #name='linear')
+            #Gate layer
+            conv_v = self.conv_op(padded_input, shape, "gated")
+            #conv_v = tf.layers.conv2d(h, filter_size,
+                                   #kernel_size=(height, width),
+                                   #strides=(1, width), padding='same',
+                                    #name='gate')
+            #Elementwise multiplication
+            #batch_size, max_len, 1, filter_size
+            h = conv_w * tf.sigmoid(conv_v)
+            #print(h)
         return h
 
     def create_embeddings(self, X, conf):
@@ -114,8 +113,8 @@ class GatedCNN(object):
         #In the original paper, the first k-1 word will be padded
         #In a convolutional network, the kernel size is k, if we choose padding mode as 'SAME'
         #That means k/2 will be padded
-        mask_size = int(conf.filter_h/2)
-        mask_layer[:,0:mask_size,:] = 0
+        #mask_size = int(conf.filter_h/2)
+        #mask_layer[:,0:mask_size,:] = 0
         embed *= mask_layer
         
         #embed_shape = embed.get_shape().as_list()
@@ -126,6 +125,9 @@ class GatedCNN(object):
 
 
     def conv_op(self, fan_in, shape, name):
+        '''
+        Depthwise convolution layer
+        '''
         W = tf.get_variable("%s_W"%name, shape, tf.float32, tf.random_normal_initializer(0.0, 0.1))
         b = tf.get_variable("%s_b"%name, shape[-1], tf.float32, tf.constant_initializer(1.0))
         return tf.add(tf.nn.depthwise_conv2d(fan_in, W, strides=[1,1,1,1], padding='VALID'), b)
